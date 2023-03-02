df09f7bb125c1517d88c0566b2f1c906516ab490
lkscomk <100165295+lkscomk@users.noreply.github.com>
2023-03-01 20:10:50 -0400

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = 'storesumbrella@gmail.com'
-app.config['MAIL_PASSWORD'] = 'jrvxsgvryrqwxswe'
+app.config['MAIL_PASSWORD'] = ''
 
 mail = Mail(app)
 
